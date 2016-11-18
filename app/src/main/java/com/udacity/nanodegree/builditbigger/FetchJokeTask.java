c72495dba809cbcef1830950dd79d8d75cdcc9bf
Nibedit <nibedit.dev@gmail.com>
2016-11-18 13:42:12 +0530

    added appID

--- app/src/main/java/com/udacity/nanodegree/builditbigger/FetchJokeTask.java
+++ app/src/main/java/com/udacity/nanodegree/builditbigger/FetchJokeTask.java
@@ -29,18 +29,8 @@ public class FetchJokeTask extends AsyncTask<Void, Void, String> {
     protected String doInBackground(Void... params) {
         if(myApiService == null) {  // Only do this once
 
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://192.168.2.7:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://android-app-backend.appspot.com/_ah/api/");
             // end options for devappserver
                 myApiService = builder.build();
         }
